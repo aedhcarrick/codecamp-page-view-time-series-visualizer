5d7cad89210431f24170cf362159c08f7caf60c4
aedh carrick <aedhcarrick@gmail.com>
2024-03-18 21:20:23 +0000

    break time.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,32 +5,32 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 
 # Clean data
-df = None
-
+df = df.loc[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    # The title should be Daily freeCodeCamp Forum Page Views 5/2016-12/2019. 
+    # The label on the x axis should be Date and the label on the y axis should be Page Views.   
+    fig, ax = plt.subplots()
+    ax.plot(df.index, df['value'])
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
-
     # Draw bar plot
-
-
-
-
+    # Copy and modify data for monthly bar plot
+    # It should show average daily page views for each month grouped by year. 
+    # The legend should show month labels and have a title of Months. 
+    # On the chart, the label on the x axis should be Years and the label on the y axis should be Average Page Views.
+    df_bar = df.copy()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
